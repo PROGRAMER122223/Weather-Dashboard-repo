1e3084b9dfc15de673a8b2e1859b39ec8baa13e7
Prathibha <pratibha.indel@gmail.com>
2022-02-02 03:58:51 -0500

    added function call to forecast function

--- script.js
+++ script.js
@@ -20,7 +20,9 @@ function find(c){
   return 1;
 }
 //SET the API key
-var api_key="473a19f6022648369699b1dffb273802";
+var api_key="a0aca8a89948154a4182dcecc780b513";
+
+
 
 
 function displayWeather(event){
@@ -55,11 +57,32 @@ var queryURL="https://api.openweathermap.org/data/2.5/weather?q=" +city + "&APPI
             
              var windsmph=(data.wind.speed*2.237).toFixed(1);
              $(currentWind).html(windsmph+"MPH");
-             console.log(currentWind+"wind");
+             //console.log(currentWind+"wind");
              //humidity
              
              $(currentHumidity).html(data.main.humidity+"%");
              UVIndex(data.coord.lon,data.coord.lat);
+             forecast(data.id);
+        if(data.cod==200){
+            city1=JSON.parse(localStorage.getItem("cityName"));
+            console.log(city1);
+            if (city1==null){
+            
+                city1=[];
+                city1.push(city.toUpperCase()
+                );
+                localStorage.setItem("cityName",JSON.stringify(city1));
+                addToList(city);
+            }
+            else {
+                if(find(city)>0){
+                    city1.push(city.toUpperCase());
+                    localStorage.setItem("cityName",JSON.stringify(city1));
+                    addToList(city);
+                }
+            }
+        }
+
         
 });
 }
@@ -75,41 +98,40 @@ function UVIndex(ln,lt){
 
 
 //displaying 5 days forecast of the city
-/*function forecast(cityid){
-    var dayOver= false;
+function forecast(cityid){
+    //var dayOver= false;
     var queryforcastURL="https://api.openweathermap.org/data/2.5/forecast?id="+cityid+"&appid="+api_key;
-    fetch(queryforcastURL)
-    .then(function (response) {
-      if (response.ok) {
-        //console.log(response);
-        response.json().then(function (data) {
-          //console.log(data);
-              for(i=0;i<5;i++){
-                  var date=new Date((data.list[((i+1)*8)-1].dt)*1000).toLocaleDateString();
+   
+    /*fetch(queryforcastURL)
+    .then (response=>response.json())
+    .then(data=>{
+        console.log(data+"helooo");*/
+    
+        $.ajax({
+            url:queryforcastURL,
+            method:"GET"
+        }).then(function(response){
+        console.log(response);
+             for(i=0;i<5;i++){
+                  var date =new Date((data.list[((i+1)*8)-1].dt)*1000).toLocaleDateString();
                   var iconcode= data.list[((i+1)*8)-1].weather[0].icon;
                   var iconurl="https://openweathermap.org/img/wn/"+iconcode+".png";
                   var tempK= data.list[((i+1)*8)-1].main.temp;
                    var tempF=(((tempK-273.5)*1.80)+32).toFixed(2);
-                   var windW=
+                
                    var humidity= data.list[((i+1)*8)-1].main.humidity;
+
+                   $("#fDate"+i).html(date);
+                   $("#fImg"+i).html("<img src="+iconurl+">");
+                   $("#fTemp"+i).html(tempF+"&#8457");
+                   $("#fHumidity"+i).html(humidity+"%");
+               
         
               }
 
 
           
         });
-      } else {
-        alert('Error: ' + response.statusText);
-      }
-    })
-    .catch(function (error) {
-      alert('Unable to connect to url');
-    });
-};
-
-*/
-
-
-
+      } 
 
 $(".btn").on("click",displayWeather);
